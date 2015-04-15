2ba2d3b181ccdeffbed3c678f9d46d76934514ac
Brendan <brendanmlahey@gmail.com>
2015-04-15 09:13:55 -0400

    Update Map.java

--- Map.java
+++ Map.java
@@ -1,116 +1,63 @@
-import javax.swing.*;
-import java.awt.* ; 
-import java.awt.event.*; 
-import javax.imageio.*;
-import java.io.*;
-import java.awt.image.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Map extends JPanel
-{
-  // view of the map
-  int[][] map = new int[][] {
-    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 3, 3, 2, 2, 3, 3, 0, 0, 0, 0, 0, 0, 0, 3, 3, 2, 2, 3, 3, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-  };
-  boolean obstacle[][] = new boolean[25][25];
+public class Player {
+  private int x, y, health, angle;
+  private double speed;
+  private Weapon gun;
   
-  Player p1 = new Player(100,100);
-  
-  public Map() {
-    KeyListener listen = new KeyListener() {
-      @Override
-      public void keyReleased(KeyEvent e) {}
-      @Override
-      public void keyTyped(KeyEvent e) {}
-      @Override
-      public void keyPressed(KeyEvent e) {
-        p1.keyPressed(e);
-      }
-    };
-    addKeyListener(listen);
-    setFocusable(true);
+  public Player(int x, int y) {
+    this.x = x;
+    this.y = y;
+    speed = 1;
+    gun = new Weapon();
+    angle = 0;
+    health = 10;
   }
   
-  public static void main(String []args) throws InterruptedException
-  {
-    //Creates the window
-    JFrame frame = new JFrame("Map");
-    Map m = new Map();
-    
-    frame.add(m);
-    
-    //Window settings
-    frame.setSize(700, 725);
-    frame.setVisible(true);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-    frame.setResizable(false);
-    
-    while(true) {
-      m.repaint();
-      Thread.sleep(5);
-    }
+  public void keyPressed(KeyEvent e) {
+    if(e.getKeyCode() == KeyEvent.VK_RIGHT)
+      turn(1);
+    if(e.getKeyCode() == KeyEvent.VK_LEFT)
+      turn(-1);
+    if(e.getKeyCode() == KeyEvent.VK_UP)
+      move();
   }
   
-  @Override
-  public void paint(Graphics g)
-  {
-    //draws map
-    super.paint(g);
+  public int getAngle(){
+    return angle;
+  }
+  
+  public void shoot() {
+    
+  }
+  
+  public void move() {
+    y += 4 * Math.sin(Math.toRadians(angle));
+    x += 4 * Math.cos(Math.toRadians(angle));
+  }
+  
+  public void changeHealth() {
+    
+  }
+  
+  public void changeWeapon() {
+    
+  }
+  
+  public void turn(int d) {
+    angle += d == 1 ? 15 : -15;
+  }
+  
+  public void paint(Graphics g) {
     Graphics2D g2d = (Graphics2D) g;
-    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-    for(int i = 0; i < 25; i++)
-    {
-      for(int j = 0; j < 25; j++)
-      {
-        if(map[j][i] == 0)
-        {
-          // colours background
-          g2d.setColor(Color.GREEN);
-          g2d.fillRect(i * 28, j * 28, 27, 27);         
-        }
-        if(map[j][i] == 1)
-        {
-          //colours wall
-          g2d.setColor(Color.BLACK);
-          g2d.fillRect(i * 28, j * 28, 27, 27);
-        }
-        if(map[j][i] == 2)
-        {
-          //colours bridge
-          g2d.setColor(new Color(165, 42, 42));
-          g2d.fillRect(i * 28, j * 28, 27, 27);
-        }
-        if(map[j][i] == 3)
-        {
-          //colours water
-          g2d.setColor(Color.BLUE);
-          g2d.fillRect(i * 28, j * 28, 27, 27);
-        } 
-      }
-    }
-    p1.paint(g2d);
+    g2d.setColor(Color.darkGray);
+    
+    g2d.rotate(Math.toRadians(angle), x + 10, y + 10);
+    g2d.fillOval(x,y,20,20);
+    g2d.fillRect(x+5,y+5,20,10);    
+    g2d.rotate(Math.toRadians(-angle), x + 10, y + 10);
+    g2d.setColor(Color.green);
+    g2d.fillArc(x,y,20,20,0,36*health);
   }
 }
