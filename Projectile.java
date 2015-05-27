b22dd5c37a10ca084e30e3fe7994d884f746e1ac
hunterliam <hunter.schofield@rogers.com>
2015-05-27 00:00:41 -0400

    Update Projectile.java

--- Projectile.java
+++ Projectile.java
@@ -7,40 +7,98 @@ import java.awt.image.*;
 
 public class Projectile
 {
+  private Map m;
   private int damage;
-  private int x;
-  private int y;
-  private int angle;
-  private Weapon weapon;
+  public int x;
+  public int y;
+  private boolean first;
+  private int angle, type;
+  private Weapon gun;
+  private int map[][];
   
-  public Projectile(int damage, int x, int y, int angle)
+  public Projectile(Weapon gun, int[][] map, boolean first, int type)
   {
-    this.damage = damage;
-    this.x = x;
-    this.y = y;
-    this.weapon = weapon;
-    this.angle = angle;
-  }
-  
-  public void collision()
-  {
-  
+    this.gun = gun;
+    this.map = map;
+    this.first = first;
+    this.type = type;
+    
+    x = gun.getX()+7;
+    y = gun.getY()+7;
+    angle = gun.getAngle();
+    if(type == 3)
+    {
+      angle = gun.getAngle() + (int)(Math.random() * (12 - (-10)) -10);
+    }
+    damage = gun.getDamage();
   }
   
   public int getAngle()
   {
-    return weapon.getAngle();
+    return gun.getAngle();
   }
   
   public void move()
   {
-    x += 2 * (int) Math.sin(Math.toRadians(angle));
-    y += 2 * (int) Math.cos(Math.toRadians(angle));
+    if((x < 710 && x > -10) && (y < 710 && y > -10))
+    {
+      if(type == 1 || type == 2 || type == 4)
+      {
+        x += 8 * Math.cos(Math.toRadians(angle));
+        y += 8 * Math.sin(Math.toRadians(angle));
+      }
+      else if(type == 3)
+      {
+        x += (Math.random() * (8 - 6) + 6) * Math.cos(Math.toRadians(angle));
+        y += (Math.random() * (8 - 6) + 6) * Math.sin(Math.toRadians(angle));
+      }
+      else if(type == 5)
+      {
+        x += 16 * Math.cos(Math.toRadians(angle));
+        y += 16 * Math.sin(Math.toRadians(angle));
+      }
+    }
+    if(map[y / 28][x / 28] == 1)
+    {
+      x = -20;
+      y = -20;
+    }
+  }
+  
+  public double getOtherX()
+  {
+    if(first == true)
+    {
+      return m.p2.x;
+    }
+    else
+    {
+      return m.p1.x;
+    }
+  }
+  
+  public double getOtherY()
+  {
+    if(first == true)
+    {
+      return m.p2.y;
+    }
+    else
+    {
+      return m.p1.y;
+    }
   }
   
   public void paint(Graphics2D g)
   {
     g.setColor(Color.yellow);
-    g.fillRect(x, y, 20, 40);
+    if(type == 4)
+    {
+      g.fillOval(x, y, 20, 20);    
+    }
+    else
+    {
+    g.fillOval(x, y, 6, 6);
+    }
   }
 }
