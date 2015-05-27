73d301ffecc36724e7464e075d4f12483b7d2a7b
hunterliam <hunter.schofield@rogers.com>
2015-05-26 23:59:59 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,34 +1,55 @@
-import java.awt.*;
-import java.awt.event.*;
-import java.util.*;
 import javax.swing.*;
 import javax.swing.Timer;
+import java.util.*;
+import java.awt.* ; 
+import java.awt.event.*; 
+import javax.imageio.*;
+import java.io.*;
+import java.awt.image.*;
 
-public class Player {
-  private int health, angle;
-  private double x, y;
+public class Player implements ActionListener
+{
+  private int health, angle, spawnCount;
+  public double x, y;
   private double speed;
-  private Weapon gun;
   private int[][] map;
   private boolean first = true;
+  private boolean canHit = true;
+  Weapon gun;
+  Map m;
+  Timer iF;
   
-  public Player(int x, int y, int[][] map, boolean first) {
+  public Player(int x, int y, int[][] map, boolean first, int spawnCount) 
+  {
     this.x = x;
     this.y = y;
+    this.map = map;
+    this.spawnCount = spawnCount;
     speed = 1;
-    gun = new Weapon(this, 1);
+    gun = new Weapon(this, 1, map, first);
     angle = 0;
     health = 10;
-    this.map = map;
     this.first = first;
+    iF = new Timer(200, this);
   }
   
+  public int getX()
+  {
+    return (int)x;
+  }
+  
+  public int getY()
+  {
+    return (int)y;
+  }
 
-  public int getAngle(){
+  public int getAngle()
+  {
     return angle;
   }
   
-  public void shoot() {
+  public void shoot()
+  {
     gun.fire();
   }
   
@@ -38,9 +59,41 @@ public class Player {
     x += Math.cos(Math.toRadians(angle));
     if(map[(int)(y / 28)][(int)(x / 28)] == 1 || map[(int)(y + 26) / 28][(int)(x + 26) / 28] == 1)
     {
-      y -= vel * Math.sin(Math.toRadians(angle));
-      x -= vel * Math.cos(Math.toRadians(angle));
+      y -= Math.sin(Math.toRadians(angle));
+      x -= Math.cos(Math.toRadians(angle));
     }
+    if(map[(int)(y / 28)][(int)(x / 28)] == 4)
+    {
+      map[(int)y / 28][(int)x / 28] = 0;
+      gun = new Weapon(this, (int)((Math.random() * (6 - 1)) + 1), map, first);
+      spawnCount--;
+    }
+    if(map[(int)(y + 26) / 28][(int)(x + 26) / 28] == 4)
+    {
+      map[(int)(y + 26) / 28][(int)(x + 26) / 28] = 0;
+      gun = new Weapon(this, (int)((Math.random() * (6 - 1)) + 1), map, first);
+      spawnCount--;
+    }
+  }
+  
+  public void hit(int damage)
+  {
+    if(canHit == true)
+    {
+      health = health - damage;
+      canHit = false;
+      iF.start();
+    }
+    if(health <= 0)
+    {
+      
+    }
+  }
+  
+  public void actionPerformed(ActionEvent a)
+  {
+    canHit = true;
+    iF.stop();
   }
   
   public boolean changeHealth(boolean down) {
@@ -51,14 +104,13 @@ public class Player {
       return false;
   }
   
-  public void changeWeapon() 
+  public void changeWeapon(int type) 
   {
-    
+    gun = new Weapon(this, type, map, first);
   }
   
-  public void turn(int d)  //, HashSet pressedKeys) 
+  public void turn(int d)
   {
-    //if(pressedKeys.contains(37) || pressedKeys.contains(39))
       angle += d == 1 ? 3 : -3;   
   }
   
@@ -72,6 +124,7 @@ public class Player {
     g2d.rotate(Math.toRadians(-angle), x + 10, y + 10);
     g2d.setColor(Color.green);
     g2d.fillArc((int)x,(int)y,20,20,0,36*health);
+    gun.paint(g2d);
   }
 }
 
